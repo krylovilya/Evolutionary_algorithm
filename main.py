2f4e94e63a58afeb4465beea8b7f629429fd321c
krylovilya1 <krylovilya1@gmail.com>
2020-05-17 14:03:28 +0800

    use pyqt5

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
-from collections import OrderedDict
+import sys
 import time
+import warnings
+from collections import OrderedDict
+
+import mainwindow
 import numpy as np
+from PyQt5 import QtWidgets
 
 
-#   Индивид
 class Individual:
     def __init__(self, x=None):
         if x is None:
@@ -28,7 +32,9 @@ class Individual:
         return 100 * (xx[0] - xx[1]) * (xx[0] - xx[1]) + (1 - x[0]) * (1 - x[0])
 
     def fitness_function(self):
-        return 1 / self.target_function()
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return 1 / self.target_function()
 
     def mutation(self):
         pos = random.randint(0, 11)
@@ -49,7 +55,6 @@ class Individual:
         return self.get_phenotype().__str__()
 
 
-#   Популяция
 class Population:
     def __init__(self, n=100):
         self.n = n
@@ -58,9 +63,6 @@ class Population:
     def gen_start_pop(self):
         for i in range(self.n):
             self.vector.append(Individual())
-
-    def resize_pop(self, new_n):
-        self.n = new_n
 
     def __len__(self):
         return self.vector.__len__()
@@ -79,16 +81,17 @@ class Population:
 
 
 class Algorithm:
-    def __init__(self):
+    def __init__(self, crossover_chance, mutation_chance, stop_criterion, print_to_console):
         self.pop = Population()
         self.pop.gen_start_pop()
-        self.crossover_chance = 0.6
-        self.mutation_chance = 0.01
+        self.stop_criterion = stop_criterion
+        self.crossover_chance = crossover_chance
+        self.mutation_chance = mutation_chance
         self.iter = 0
-        self.stop_criterion = 200
         self.max_fitness = None
         self.unchanged = 0
         self.time = None
+        self.print_to_console = print_to_console
 
     def rank_selection(self):
         old_population = self.pop
@@ -97,22 +100,17 @@ class Algorithm:
         for i, key in enumerate(fitness):
             fitness[key][1] = i + 1
             sum_ranks += i + 1
-        # fitness = OrderedDict({el.fitness_function(): i for i, el in enumerate(self.pop.vector)})
-        # p = OrderedDict({fitness[key][0]: fitness[key][1] / sum_ranks for key in fitness})
         new_fitness = OrderedDict({fitness[key][0]: fitness[key][1] for key in fitness})
-        # cumulate = 0
-        # rnd = [random.random() for i in range(200)]
-
-        n = [round(2 * ((new_fitness[key] - 1) / (100 - 1))) for key in new_fitness]
+        n = [round(2 * ((new_fitness[key] - 1) / (self.pop.n - 1))) for key in new_fitness]
         new_population = Population()
         fitness_iter = reversed(new_fitness)
         for _n in list(reversed(n)):
             current_obj = next(fitness_iter)
             for j in range(_n):
                 new_population.vector.append(current_obj)
-            if _n == 0 and len(new_population.vector) < 100:
+            if _n == 0 and len(new_population.vector) < self.pop.n:
                 new_population.vector.append(current_obj)
-        while len(new_population.vector) < 100:
+        while len(new_population.vector) < self.pop.n:
             new_population.vector.append(random.choice(new_population.vector))
         self.pop = new_population
 
@@ -120,7 +118,7 @@ class Algorithm:
         old_population = self.pop
         old_population.vector = random.sample(old_population.vector, len(old_population.vector))
         new_population = Population()
-        pairs = list(zip(old_population.vector[:50], old_population.vector[50:]))
+        pairs = list(zip(old_population.vector[:int(self.pop.n / 2)], old_population.vector[int(self.pop.n / 2):]))
         for pair in pairs:
             do_crossover = random.random() <= self.crossover_chance
             if do_crossover:
@@ -141,15 +139,24 @@ class Algorithm:
         self.pop = new_population
 
     def mutation(self):
-        rnd = [random.random() for i in range(100)]
+        rnd = [random.random() for i in range(self.pop.n)]
         for _rnd, el in zip(rnd, self.pop.vector):
             if _rnd <= self.mutation_chance:
                 el.mutation()
 
     def stop_function(self):
-        condition1 = self.unchanged > self.stop_criterion
-        condition2 = self.pop.get_max() == np.inf
-        condition3 = time.time() - self.time > 2
+        if self.stop_criterion[0]:
+            condition1 = self.unchanged > self.stop_criterion[0] - 1
+        else:
+            condition1 = False
+        if self.stop_criterion[1]:
+            condition2 = np.isinf(self.pop.get_max())
+        else:
+            condition2 = False
+        if self.stop_criterion[2]:
+            condition3 = time.time() - self.time > self.stop_criterion[2]
+        else:
+            condition3 = False
         return condition1 or condition2 or condition3
 
     def start(self):
@@ -157,38 +164,77 @@ class Algorithm:
         self.max_fitness = self.pop.get_max()
         result = None
         while True:
-            print(self.iter, self.unchanged, self.pop.get_statistics())
+            if self.print_to_console:
+                print(self.iter, self.unchanged, self.pop.get_statistics())
             stop = self.stop_function()
             if not stop:
                 self.iter += 1
                 self.rank_selection()
                 self.single_point_crossover()
                 self.mutation()
-                if self.pop.get_max() > self.max_fitness:
-                    self.max_fitness = self.pop.get_max()
+                max_value = self.pop.get_max()
+                if max_value > self.max_fitness or \
+                        not np.isinf(max_value) and np.isinf(self.max_fitness):
+                    self.max_fitness = max_value
                     self.unchanged = 0
                 else:
                     self.unchanged += 1
             else:
-                # result = self.pop.vector[max(range(len(self.pop.vector)), key=self.pop.vector.__getitem__)]
                 result = self.pop.vector[np.argmax(map(Individual.fitness_function, self.pop.vector))]
                 break
         return result.get_phenotype()
 
 
-if __name__ == "__main__":
-    a = Algorithm()
-    print(a.start())
-    # print(len(a.pop))
-    # for i in range(100):
-    #    a.rank_selection()
-    #    print(i, '\t',a.pop.get_statistics())
-    #    a.single_point_crossover()
-    #    print('\t', a.pop.get_statistics())
-    #    a.mutation()
-    #    print('\t', a.pop.get_statistics())
-    # print(list(map(Individual.get_phenotype, pop.vector)))
+class MainApp(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
-#    pop = Population()
-#    for i, el in enumerate(pop.vector):
-#        print(i + 1, el.target_function(), el.fitness_function())
+    def button_click(self):
+        self.pushButton.setEnabled(False)
+        crossover_chance = self.doubleSpinBox.value() / 100
+        mutation_chance = self.doubleSpinBox_2.value() / 100
+        stop_criterion = []
+        if self.checkBox.isChecked():
+            stop_criterion.append(self.spinBox.value())
+        else:
+            stop_criterion.append(False)
+        stop_criterion.append(self.checkBox_2.isChecked())
+        if self.checkBox_3.isChecked():
+            stop_criterion.append(self.doubleSpinBox_3.value())
+        else:
+            stop_criterion.append(False)
+        if not any(stop_criterion):
+            QtWidgets.QMessageBox.critical(self, "Ошибка", "Необходим хотя бы один критерий останова!")
+            self.pushButton.setEnabled(True)
+            return
+        print_to_console = self.checkBox_4.isChecked()
+        a = Algorithm(crossover_chance, mutation_chance, stop_criterion, print_to_console)
+        result = a.start()
+        if print_to_console:
+            print("\n 1 значение: номер популяции\n",
+                  "2 значение: количество поколений без улучшений\n",
+                  "3 значение: индивид с минимальным значением функции приспособленности в популяции\n",
+                  "4 значение: индивид со средним значением функции приспособленности в популяции\n",
+                  "5 значение: индивид с максимальным значением функции приспособленности в популяции\n")
+        self.label_7.setText(str(result)[1:-1])
+        self.label_9.setText(str(a.iter))
+        self.label_11.setText(str(round(time.time() - a.time, 4)) + " с.")
+        self.pushButton.setEnabled(True)
+
+    def spinbox_changed(self, new_val):
+        self.checkBox.setText(new_val.__str__() + " поколений без улучшений")
+
+    def time_spinbox_changed(self, new_val):
+        self.checkBox_3.setText(str(new_val) + " сек. процессорного времени")
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == "__main__":
+    main()
